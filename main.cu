74b4aa41f93e152219f6bf7575240ff4f06f9d1d
enptfb55 <enptfb55@users.noreply.github.com>
2017-04-11 03:30:38 +0000

    added some gpu stuff

---
+++ main.cu
@@ -0,0 +1,82 @@
+
+#include <iostream>
+#include <thread>
+#include <cstdlib>
+#include <cstring>
+#include <getopt.h>
+
+#include <time.h>
+
+#include <curand.h>
+
+#include "monte_carlo_cpu.h"
+#include "monte_carlo_gpu.h"
+
+void usage(const char* app)
+{
+    std::cout << "Usage: " << ::basename(app) << " [-h] config_path" << std::endl;
+    std::cout << "Options:\n"
+              << "  -h, --help              This help message\n"
+              << "  config_path             Path to config file\n"
+              << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    /*
+    if (argc == 1) {
+        std::cerr << ::basename(argv[0]) << ": missing option(s)\n" << std::endl;
+        ::usage(argv[0]);
+        return 1;
+    }
+    */
+
+    while (true) {
+        static option long_options[] = {
+            {"help", no_argument, NULL, 'h'},
+            {0, 0, 0, 0},
+        };
+
+        const int c = ::getopt_long(argc, argv, "h", long_options, nullptr);
+        if (c == -1) break;
+
+        switch (c) {
+            case 'h':
+            case '?':
+            default:
+            ::usage(argv[0]);
+            return 1;
+        }
+    } 
+
+    //std::string config_path(argv[1]);
+
+    size_t num_paths = 100000;   // Number of simulated asset paths
+    size_t num_steps = 365;
+    size_t num_normals = num_paths * num_steps;
+    double S = 143.34;  // Option price
+    double K = 119.0;  // Strike price
+    double r = 0.10304;   // Risk-free rate (5%)
+    double v = 0.48;    // Volatility of the underlying (20%)
+    double T = 1/12;    // One year until expiry
+    float dt = float(T)/float(num_steps);
+    float sqrdt = sqrt(dt);
+
+    DeviceArray<float> d_normals(num_normals);
+
+    curandGenerator_t curandGenerator;
+    curandCreateGenerator(&curandGenerator, CURAND_RNG_PSEUDO_MTGP32);
+    curandSetPseudoRandomGeneratorSeed(curandGenerator, 1234ULL) ;
+    curandGenerateNormal(curandGenerator, d_normals.get_data(), num_normals, 0.0f, sqrdt);
+
+    std::vector<float> h_normals(num_normals);
+    d_normals.get(&h_normals[0], num_normals);
+
+    MonteCarloCPU monte_carlo_cpu(h_normals);
+    monte_carlo_cpu.calculate(num_normals, S, K, r, v, T);
+
+    MonteCarloGPU monte_carlo_gpu(&d_normals);
+    monte_carlo_gpu.calculate(num_normals, num_paths, S, K, r, v, T);
+
+    return 0;
+}
--- monte_carlo_cpu.h
+++ monte_carlo_cpu.h
@@ -6,90 +6,76 @@
 #include <cmath>
 #include <iostream>
 #include <random>
+#include <vector>
 
 #include "monte_carlo_common.h"
 
 class MonteCarloCPU
 {
-private:
-	std::random_device m_rd;
-    std::mt19937 m_gen;
-    std::normal_distribution<> m_dist;
+    std::vector<float> m_normals;
 
 public:
-	MonteCarloCPU()
-		: m_rd(),
-		  m_gen(m_rd()),
-		  m_dist(70,10)
-	{
-	}
+    explicit MonteCarloCPU(const std::vector<float>& _normals)
+            : m_normals(_normals)
+    {
+    }
 
 private:
-	double call_price(const int& num_sims, const double& S, const double& K, const double& r, const double& v, const double& T) 
-	{
-		double S_adjust = S * exp(T*(r-0.5*v*v));
-		double S_cur = 0.0;
-		double payoff_sum = 0.0;
+    double call_price(const int& num_sims, const double& S, const double& K, const double& r, const double& v, const double& T) 
+    {
+        double S_adjust = S * exp(T*(r-0.5*v*v));
+        double S_cur = 0.0;
+        double payoff_sum = 0.0;
 
-		for (int i=0; i<num_sims; i++) {
-			double rand_num = m_dist(m_gen);
-			S_cur = S_adjust * exp(sqrt(v*v*T)*rand_num);
-			payoff_sum += std::max(S_cur - K, 0.0);
-		}
+        for (int i=0; i<num_sims; i++) {
+            S_cur = S_adjust * exp(sqrt(v*v*T)*m_normals[i]);
+            payoff_sum += std::max(S_cur - K, 0.0);
+        }
 
-		return (payoff_sum / static_cast<double>(num_sims)) * exp(-r*T);
-	}
+        return (payoff_sum / static_cast<double>(num_sims)) * exp(-r*T);
+    }
 
-	double put_price(const int& num_sims, const double& S, const double& K, const double& r, const double& v, const double& T) 
-	{
-		double S_adjust = S * exp(T*(r-0.5*v*v));
-		double S_cur = 0.0;
-		double payoff_sum = 0.0;
+    double put_price(const int& num_sims, const double& S, const double& K, const double& r, const double& v, const double& T) 
+    {
+        double S_adjust = S * exp(T*(r-0.5*v*v));
+        double S_cur = 0.0;
+        double payoff_sum = 0.0;
 
-		for (int i=0; i<num_sims; i++) {
-			double rand_num = m_dist(m_gen);
-			S_cur = S_adjust * exp(sqrt(v*v*T)*rand_num);
-			payoff_sum += std::max(K - S_cur, 0.0);
-		}
+        for (int i=0; i<num_sims; i++) {
+            S_cur = S_adjust * exp(sqrt(v*v*T)*m_normals[i]);
+            payoff_sum += std::max(K - S_cur, 0.0);
+        }
 
-		return (payoff_sum / static_cast<double>(num_sims)) * exp(-r*T);
-	}
+        return (payoff_sum / static_cast<double>(num_sims)) * exp(-r*T);
+    }
 
 public:
-	void calculate()
-	{
-	// First we create the parameter list
-    int num_sims = 10000000;   // Number of simulated asset paths
-    double S = 143.34;  // Option price
-    double K = 119.0;  // Strike price
-    double r = 0.10304;   // Risk-free rate (5%)
-    double v = 0.48;    // Volatility of the underlying (20%)
-    double T = 1/12;    // One year until expiry
+    void calculate(size_t num_sims, double S, double K, double r, double v, double T)
+    {
+        struct timespec start_ts, end_ts;
 
-    struct timespec start_ts, end_ts;
+        clock_gettime(CLOCK_MONOTONIC, &start_ts);  
 
-    clock_gettime(CLOCK_MONOTONIC, &start_ts);  
+        // Then we calculate the call/put values via Monte Carlo
+        double call = call_price(num_sims, S, K, r, v, T);
+        double put = put_price(num_sims, S, K, r, v, T);
 
-    // Then we calculate the call/put values via Monte Carlo
-    double call = call_price(num_sims, S, K, r, v, T);
-    double put = put_price(num_sims, S, K, r, v, T);
-
-    clock_gettime(CLOCK_MONOTONIC, &end_ts);
+        clock_gettime(CLOCK_MONOTONIC, &end_ts);
 
 
-    // Finally we output the parameters and prices
-    std::cout << "Number of Paths:      " << num_sims << std::endl;
-    std::cout << "Underlying:           " << S << std::endl;
-    std::cout << "Strike:               " << K << std::endl;
-    std::cout << "Risk-Free Rate:       " << r << std::endl;
-    std::cout << "Volatility:           " << v << std::endl;
-    std::cout << "Maturity:             " << T << std::endl;
+        std::cout << "==== CPU Results =====" << std::endl;
+        std::cout << "Number of Paths:      " << num_sims << std::endl;
+        std::cout << "Underlying:           " << S << std::endl;
+        std::cout << "Strike:               " << K << std::endl;
+        std::cout << "Risk-Free Rate:       " << r << std::endl;
+        std::cout << "Volatility:           " << v << std::endl;
+        std::cout << "Maturity:             " << T << std::endl;
 
-    std::cout << "Call Price:           " << call << std::endl;
-    std::cout << "Put Price:            " << put << std::endl;
+        std::cout << "Call Price:           " << call << std::endl;
+        std::cout << "Put Price:            " << put << std::endl;
 
-    std::cout << "Time Elapsed(ms):     " << diff_ts_us(end_ts, start_ts) / 1000 << std::endl;
+        std::cout << "Time Elapsed(ms):     " << diff_ts_us(end_ts, start_ts) / 1000 << std::endl;
 
-	}
+    }
 
 };
